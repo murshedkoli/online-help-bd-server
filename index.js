20766a328eceff389d2abd4e118b6b8d7254fcb5
Murshed Al Main <murshedkoli@gmail.com>
2021-04-29 23:42:16 +0600

    add place order funtionality and update order funtionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
 const uri = "mongodb+srv://ababilit:bYpBAUmDZ4sGf8Y@cluster0.dmtd1.mongodb.net/online-help-bd?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -50,7 +51,7 @@ client.connect(err => {
         console.log(req.body)
         ordersCollection.insertOne(req.body)
             .then(result => {
-               console.log(result)
+               res.send(result)
 
             })
 
@@ -65,7 +66,82 @@ client.connect(err => {
             })
     })
 
+    app.get('/orders', (req, res) => {
+        ordersCollection.find()
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
+
+    app.get('/pendingorders', (req, res) => {
+        ordersCollection.find({status:'pending'})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/completeorders', (req, res) => {
+        ordersCollection.find({status:'complete'})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/singleorder', (req, res) => {
+        ordersCollection.find({_id:ObjectId(req.query.id)})
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+
+    
+    app.get('/loggedUser', (req, res) => {
+        usersCollection.find({email: req.query.email})
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+
+     
+  app.patch('/confirm/:id', (req, res) => {
+    const id=req.params.id;
+    const data= req.body;
+    
+   usersCollection.updateOne({_id: ObjectId(id)},
+    {$set:{ balance: data.ammount }}
+    )
+    .then(result=>{
+      res.send(result)
+      
+    })
+   
+   
+  })
+
+
+
+
+  app.patch('/updatestatus/:id', (req, res) => {
+    const id=req.params.id;
+    const data= req.body;
+    
+   ordersCollection.updateOne({_id: ObjectId(id)},
+    {$set:{ status: data.orderStatus }}
+    )
+    .then(result=>{
+      res.send(result)
+      
+    })
+   
+   
+  })
+
+
+
+console.log('connected')
 
 });
 
